380b821b7e3cb5bd2e3bfbcc3e5d2cbc92b8880b
Eric J Wilhite <ericjwilhite@gmail.com>
2019-07-23 18:42:31 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,12 +4,12 @@ require 'pry'
 
 def key_for_min_value(name_hash)
   first_key, smallest = name_hash.first
-  binding.pry
   if !key
     answer = nil
   else
     name_hash.each do |key, value|
       if value < smallest
+        smallest = value
         answer = key
       end
   end
