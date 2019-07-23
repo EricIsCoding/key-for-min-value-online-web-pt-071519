a9e522f410cecf6112acbe1184060c1625911655
Eric J Wilhite <ericjwilhite@gmail.com>
2019-07-23 18:43:32 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,8 +3,8 @@
 require 'pry'
 
 def key_for_min_value(name_hash)
-  first_key, smallest = name_hash.first
-  if !key
+  answer, smallest = name_hash.first
+  if !answer
     answer = nil
   else
     name_hash.each do |key, value|
